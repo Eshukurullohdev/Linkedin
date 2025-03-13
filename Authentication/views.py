713111549cc56f3f03e33474f8329c67e2a6530f
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-03-13 10:29:56 +0500

    user

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Profil  
+def profil(request):
+    profil = Profil.objects.all()
+    return render(request, 'profile.html', {'profil':profil})
