59be7326405b16306a9eb2641f872f5bbc0e820a
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-03-13 09:49:28 +0500

    post create

--- LinkedIn/models.py
+++ LinkedIn/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    image = models.ImageField(upload_to='posts/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return f"Post {self.id} - {self.created_at.strftime('%Y-%m-%d %H:%M')}"
--- LinkedIn/views.py
+++ LinkedIn/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from LinkedIn.forms import PostForm
+from .models import Post
 
 
 def nav(request):
     return render(request, 'nav.html')
 
 def home(request):
-    return render(request, 'home.html')
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    else:
+        form = PostForm()
+        
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'form':form, 'posts': posts })
 
