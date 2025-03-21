10bc053e987c99f40c00e026299937c9fec59fad
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-03-21 16:40:29 +0500

    add profile

--- Authentication/admin.py
+++ Authentication/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
-# Register your models here.
+from Authentication.models import Profil
+admin.site.register(Profil)
--- LinkedIn/urls.py
+++ LinkedIn/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('home/', home, name='home'),
+    path('', home, name='home'),
     path('delete/<int:post_id>/', delete_post, name='delete_post'),
 ]
