f7805aad413d2110f10417ddd1be060b4a737fd5
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-04-07 17:41:12 +0500

    logout page

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profil
 from LinkedIn.models import Post
-# Create your views here.
+
+
 def profil(request):
     profil = Profil.objects.all()
     post = Post.objects.all()
     return render(request, 'profile.html', {'profil': profil, 'post': post})
+
+
+
+
