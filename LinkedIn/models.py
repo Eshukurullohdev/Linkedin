4256ab410c3c266d5c90f51b7932274f073a59a4
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-04-11 14:07:46 +0500

    add text for post

--- LinkedIn/models.py
+++ LinkedIn/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class Post(models.Model):
-    image = models.ImageField(upload_to='posts/')  # Rasm 'media/posts/' ichiga yuklanadi
-    created_at = models.DateTimeField(auto_now_add=True)  # Post qachon yaratilganini saqlaydi
+    image = models.ImageField(upload_to='posts/')
+    text = models.CharField(max_length=255, blank=True, null=True)# Rasm 'media/posts/' ichiga yuklanadi
+    created_at = models.DateTimeField(auto_now_add=True)# Post qachon yaratilganini saqlaydi
+
 
     def __str__(self):
         return f"Post {self.id}"
