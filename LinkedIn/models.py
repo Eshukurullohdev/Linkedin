1f680a10644dff9a91365fd83bc17252006894fb
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-04-11 17:32:29 +0500

    new code with student

--- LinkedIn/models.py
+++ LinkedIn/models.py
@@ -2,9 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     image = models.ImageField(upload_to='posts/')
-    text = models.CharField(max_length=255, blank=True, null=True)# Rasm 'media/posts/' ichiga yuklanadi
+    text = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)# Post qachon yaratilganini saqlaydi
-
-
     def __str__(self):
         return f"Post {self.id}"
