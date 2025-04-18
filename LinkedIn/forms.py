e6634da2cda15adbb5f1691f8db6f6aa870c2f05
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-04-18 22:42:55 +0500

    do not know updates

--- LinkedIn/forms.py
+++ LinkedIn/forms.py
@@ -20,32 +20,32 @@ class ProfileEditForm(forms.ModelForm):
 
         self.fields['img'].widget.attrs.update({
             'class': 'block w-full  text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 focus:outline-none file:cursor-pointer',
-        })
+        });
 
         self.fields['fullname'].widget.attrs.update({
             'class': 'mt-2 p-2 outline-none block w-full rounded-xl border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50',
             'placeholder': 'To‘liq ismingiz'
-        })
+        });
 
         self.fields['location'].widget.attrs.update({
             'class': 'mt-2 p-2 outline-none block w-full rounded-xl border-gray-300 shadow-sm focus:border-green-500 focus:ring focus:ring-green-200 focus:ring-opacity-50',
             'placeholder': 'Yashash joyingiz'
-        })
+        });
 
         self.fields['bio'].widget.attrs.update({
             'class': 'mt-2 p-2 outline-none block w-full rounded-xl border-gray-300 shadow-sm focus:border-purple-500 focus:ring focus:ring-purple-200 focus:ring-opacity-50',
             'placeholder': 'Qisqacha bio',
             'rows': 3
-        })
+        });
 
         self.fields['profilBanner'].widget.attrs.update({
             'class': 'block p-2 w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-pink-50 file:text-pink-700 hover:file:bg-pink-100',
-        })
+        });
 
         self.fields['ish'].widget.attrs.update({
             'class': 'mt-2 p-2 block outline-none w-full rounded-xl border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50',
             'placeholder': 'Ish joyingiz yoki kasbingiz'
-        })
+        });
         
     
         
